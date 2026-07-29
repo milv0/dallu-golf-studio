9477986b93070d08315d5030fa6e9354d2cf76b8
milv0 <uqwe00@gmail.com>
2026-07-30 02:51:54 +0900

    Add AdSense script and ads.txt

--- app/layout.js
+++ app/layout.js
@@ -48,6 +48,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="ko" data-theme="light" suppressHydrationWarning
       className={`${barlowCondensed.variable} ${barlow.variable} ${jetbrainsMono.variable}`}>
+      <head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4755795516057681" crossOrigin="anonymous" />
+      </head>
       <body>{children}</body>
     </html>
   );
